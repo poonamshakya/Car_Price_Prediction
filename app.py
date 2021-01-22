5cbc1adf55bf4dce47b63a333703d7ebb9e4a210
poonamshakya <38455390+poonamshakya@users.noreply.github.com>
2021-01-22 23:02:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
-def Home():
+def home():
     return render_template('index.html')
 
 
